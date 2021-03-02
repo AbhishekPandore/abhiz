b9e9395fe138e5bc25cee954d553090b5ac9a593
Admin <Admin@ABHIZ.mshome.net>
2021-03-02 15:51:59 +0530

    commit 1

--- src/Demo1.java
+++ src/Demo1.java
@@ -1,7 +1,7 @@
 
 public class Demo1 {
 	public static void main(String[] args) {
-		System.out.println("HI EEVERYONE");
+		System.out.println("HI EVERYONE");
 		System.out.println("Bye");
 		// hello from github
 		// hi 
