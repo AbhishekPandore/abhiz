c54a8b8a1aff46519281909cdd6368ec92183b34
Abhishek Pandore <78995117+AbhishekPandore@users.noreply.github.com>
2025-09-09 18:14:33 +0530

    Update Demo1.java

--- src/Demo1.java
+++ src/Demo1.java
@@ -5,11 +5,8 @@ public class Demo1 {
 		System.out.println("Bye");
 		// hello from github
 		// hi 
-		// hekko hi how r u
-		//jskhs,hsihsjsjsfskfnsknfnsnnsnndjasnsmsnfmsbdsfnsejmsnfwn
 		// ok then hi from github
 		System.out.println("HIIIII");
 	}
-	
-
 }
+
